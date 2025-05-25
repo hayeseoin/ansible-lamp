7673730ee4fc3ec24b91c001a7b49c3162a567e4
Eoin Hayes <eoahayes13@gmail.com>
2025-05-25 13:34:40 +0200

    added packages check for deploying on alma linux / other minimal distro

--- roles/php/files/install.sh
+++ roles/php/files/install.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 
+
+# export PHP_DOWNLOAD_LINK="ttps://github.com/hayeseoin/al2023-php74/raw/refs/heads/main/php-7.4.tar.gz"
+# export PHP_NAME="php-7.4.tar.gz"
+# wget -N $PHP_DOWNLOAD_LINK
+
 # Must be run as root
 if [[ "$EUID" -ne 0 ]]; then
   echo "This script must be run as root. Exiting."
   exit 1
 fi
 
-# # Ensure script is run from relative directory
-# cd "$(dirname "$0")" || {
-#   echo "Failed to change to script directory"
-#   exit 1
-# }
-
-sudo dnf install -y libicu libxslt oniguruma libedit libuuid
-sudo dnf install -y tar 
-
-wget -N $PHP_DOWNLOAD_LINK
-tar -xvf php-7.4.tar.gz -C /usr/local/
+tar -xvf $PHP_ARCHIVE -C /usr/local/
 
 rm /usr/bin/php
 rm /usr/bin/phpize
 rm /usr/bin/php-config
+
 ln -s /usr/local/php-7.4/bin/php /usr/bin/php
 ln -s /usr/local/php-7.4/bin/phpize /usr/bin/phpize
 ln -s /usr/local/php-7.4/bin/php-config /usr/bin/php-config
