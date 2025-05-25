f6d4ba603e4b2d9cca1812434487fecc16da2394
Eoin Hayes <eoahayes13@gmail.com>
2025-05-25 12:48:58 +0200

    wrapped tomcat install script in ansible, enabled upgrades and downgrades

--- roles/openjdk/files/install.sh
+++ roles/openjdk/files/install.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 
 # Set variables if not set ant env level
-# JDK_VERSION=
-# OPENJDK_DOWNLOAD_URL=
-# JAVA_INSTALL_DIR=
-# OPT_JAVA=
+# export JDK_VERSION="jdk-11.0.27+6"
+# export OPENJDK_DOWNLOAD_URL="https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.27%2B6/OpenJDK11U-jdk_x64_linux_hotspot_11.0.27_6.tar.gz
+# export JAVA_INSTALL_DIR=/usr/lib/jvm
+# export OPT_JAVA=/opt/java
+# export JAVA_ARCHIVE="OpenJDK11U-jdk_x64_linux_hotspot_11.0.27_6.tar.gz"
 # 
 # If script is not already being intiaisted from 
 # a directory with Java, download it
 # wget -N $OPENJDK_DOWNLOAD_URL
 
-set -euxo pipefail
-
-wget -N $OPENJDK_DOWNLOAD_URL
 mkdir -p $JAVA_INSTALL_DIR
-tar -xf OpenJDK11U-jdk_x64_linux_hotspot_11.0.27_6.tar.gz \
-    -C $JAVA_INSTALL_DIR/ 
+tar -xf $JAVA_ARCHIVE  -C $JAVA_INSTALL_DIR/ 
+rm -rf $OPT_JAVA 
 ln -s $JAVA_INSTALL_DIR/$JDK_VERSION $OPT_JAVA 
 echo "export JAVA_HOME=$OPT_JAVA" > /etc/profile.d/java.sh
 echo "export PATH=\$JAVA_HOME/bin:\$PATH" >> /etc/profile.d/java.sh
-# chmod +x /etc/profile.d/java.sh
-
 
 
--- roles/tomcat/files/install.sh
+++ roles/tomcat/files/install.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-# TOMCAT_ARCHIVE=
-# TOMCAT_INSTALL_LOCATION=/usr/share
-# TOMCAT_SYS_HOME=/usr/share/tomcat9
-# TOMCAT_SERVICE_FILE=tomcat9.service
+# export TOMCAT_DOWNLOAD_URL="https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.105/bin/apache-tomcat-9.0.105.tar.gz"
+# export TOMCAT_ARCHIVE="apache-tomcat-9.0.105.tar.gz"
+# export TOMCAT_INSTALL_LOCATION=/usr/share
+# export TOMCAT_SYS_HOME=/usr/share/tomcat9
+# export TOMCAT_SERVICE_FILE=tomcat9.service
 
-TOMCAT_DIR_NAME=$(basename "$TOMCAT_ARCHIVE" .tar.gz)
-TOMCAT_SERVICE_NAME=$(basename "$TOMCAT_SERVICE_FILE" .service)
+# export TOMCAT_DIR_NAME=$(basename "$TOMCAT_ARCHIVE" .tar.gz)
+# export TOMCAT_SERVICE_NAME=$(basename "$TOMCAT_SERVICE_FILE" .service)
 
-mkdir -p $TOMCAT_SYS_HOME
+# wget $TOMCAT_DOWNLOAD_URL
+
 
 if ! getent group tomcat > /dev/null; then
   groupadd tomcat
@@ -17,18 +19,22 @@ else
 fi
 
 if ! id tomcat > /dev/null 2>&1; then
-  useradd -g tomcat -s /bin/bash -c "Tomcat user" -d "$TOMCAT_SYS_HOME" 
+  useradd -g tomcat -s /bin/bash -c "Tomcat user" -d /nonexistent tomcat 
 else
   echo 'tomcat user already exists: Skipping.'
 fi
 
-tar -zxf $TOMCAT_ARCHIVE -C $TOMCAT_INSTALL_LOC
-ln -s $TOMCAT_DIR_NAME $TOMCAT_SYS_HOME
+tar -zxf $TOMCAT_ARCHIVE -C $TOMCAT_INSTALL_LOCATION/
+
+rm -rf $TOMCAT_SYS_HOME
+ln -s $TOMCAT_INSTALL_LOCATION/$TOMCAT_DIR_NAME $TOMCAT_SYS_HOME
 
 chown -R tomcat:tomcat $TOMCAT_SYS_HOME
-chown -R tomcat:tomcat $TOMCAT_DIR_NAME
+chown -R tomcat:tomcat $TOMCAT_INSTALL_LOCATION/$TOMCAT_DIR_NAME
 
-cp $TOMCAT_SERVICE_FILE /etc/systemd/system/
-
+# cp $TOMCAT_SERVICE_FILE /usr/lib/systemd/system/
+install -m 644 -D $TOMCAT_SERVICE_FILE /usr/lib/systemd/system/$TOMCAT_SERVICE_FILE
 systemctl daemon-reload
-systemctl enable $TOMCAT_SERVICE_NAME --now
+
+# systemctl daemon-reload
+# systemctl enable $TOMCAT_SERVICE_NAME --now
